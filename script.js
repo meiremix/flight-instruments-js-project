820d0c7f2dba707e2132f449c90c4f9a980be2e8
meiremix <110632650+meiremix@users.noreply.github.com>
2023-07-03 21:43:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ function updateValues(data) {
   adiElement.textContent = `ADI: ${data.adi}`;
   hisElement.textContent = `HIS: ${data.his}`;
 
+  updateAdiCircleColor(data.adi);
+  moveArrow(data.altitude);
+
 
 setTimeout(fetchData, 2000);
 }
@@ -21,3 +24,35 @@ setTimeout(fetchData, 2000);
 
 // Fetch initial data
 fetchData();
+
+
+
+
+function updateAdiCircleColor(value) {
+  const circle = document.getElementById("circle");
+  let percentage;
+
+  if (value >= 0) {
+    percentage = (value / 200 + 0.5) * 100;
+  } else {
+    percentage = 100 - (((value * (-1)) / 200 + 0.5) * 100);
+  }
+
+  circle.style.background = `linear-gradient(to bottom, blue ${percentage}%, green 0%)`;
+}
+
+
+
+function moveArrow(value) {
+  const input = value;
+  const bar = document.getElementById('bar');
+  const barArrow = document.getElementById('barArrow');
+  const parse = parseFloat(value) || 0;
+  const percentage = Math.min(100, (parse / 3000) * 100); // Calculate percentage value
+  const barHeight = bar.clientHeight;
+  const arrowHeight = barArrow.clientHeight;
+
+  const arrowTop = (barHeight - arrowHeight) * (1 - percentage / 100); // Calculate top position of the arrow
+  
+  barArrow.style.top = (arrowTop - 1) + 'px'; // Set the top position of the arrow
+}
